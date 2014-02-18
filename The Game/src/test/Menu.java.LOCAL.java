ba33b38a585cbaad35b7b79b4760c7ab9ce745a3
Jakub-Ciecierski <jakub.ciecier@gmail.com>
2014-02-18 14:55:45 +0100

    Change

--- The Game/src/test/Menu.java.LOCAL.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Menu {
-	
-	public void printMenu()
-	{
-		System.out.println("dupa");
-	}
-}
--- The Game/src/test/Menu.java.REMOTE.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class Menu {
-	public void printMenu ()
-	{
-		System.out.println("Asshole");
-	}
-}
